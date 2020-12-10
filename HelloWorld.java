6a80bdaa513589ff8326e378e57a33783caf1e02
Jayesh Gopal <jayeshg4433@outlook.com>
2020-12-10 13:14:55 +0530

    one line added

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld{
         System.out.println("Hello World From App");
         System.out.println("added another line");
         int num=10;
-        System.out.println(num);
+        System.out.println(num*10);
     }
 }
